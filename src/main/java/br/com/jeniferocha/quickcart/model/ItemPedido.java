787a0b0799cf48be45b35eabb02d464683002066
jenifer rocha <jeniferdutra2000@icloud.com>
2026-07-17 23:53:39 -0300

    feat: cria entidade ItemPedido para mapear relacionamento N:M entre Pedido e Produto

---
+++ src/main/java/br/com/jeniferocha/quickcart/model/ItemPedido.java
@@ -0,0 +1,79 @@
+package br.com.jeniferocha.quickcart.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tb_itens_pedido")
+public class ItemPedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id", nullable = false)// define o nome da coluna da chave estrangeira
+    Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id", nullable = false)
+    Produto produto;
+
+    @Column(nullable = false)
+    private Integer quantidade;
+
+    @Column(name = "preco_unitario", nullable = false)
+    private BigDecimal precoUnitario;
+
+    public ItemPedido() {
+    }
+
+    public ItemPedido(Long id, Pedido pedido, Produto produto, Integer quantidade, BigDecimal precoUnitario) {
+        this.id = id;
+        this.pedido = pedido;
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.precoUnitario = precoUnitario;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(BigDecimal precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+}
